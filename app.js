0015e9e3e00a112b34f917f39905b8088cbeeac9
manuelcanavari <manuel.canavari@betterwelltech.com>
2026-06-21 19:46:23 +0200

    v0.8 - Seguridad: login con roles y validación de credenciales

--- app.js
+++ app.js
@@ -33,3 +33,39 @@ function confirmAppointment(id) {
 function cancelAppointment(id) {
     console.log("Turno cancelado:", id);
 }
+
+// HU-02: requestAppointment() implementado
+// Feature: solicitud de turnos desde el formulario del cliente
+// Status: completado
+
+const ROLES = {
+    admin: "admin",
+    client: "client"
+};
+
+function authenticate() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if (!email || !password) {
+        document.getElementById("loginError").style.display = "block";
+        return;
+    }
+
+    const users = [
+        {email: "admin@stylehub.com", password: "admin", role: ROLES.admin},
+        {email: "client@stylehub.com", role: ROLES.client}
+    ]
+
+    const user = users.find(u => u.email === email && u.password === password);
+    if (user) {
+        console.log("Access granted. Role", user.role);
+        if (user.role === ROLES.admin){
+            window.location.href = "admin.html";
+        } else {
+            window.location.href = "index.html";
+        }
+    } else {
+        document.getElementById("loginError").style.display = "block";
+    }
+}
